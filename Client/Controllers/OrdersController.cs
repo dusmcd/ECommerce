e51e14e0a93bc3ab361854e001820b8abeaa81b0
dusmcd <dustinmcdowell1989@gmail.com>
2025-07-16 16:24:35 -0400

    fetching products for an order on the client side

---
+++ Client/Controllers/OrdersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Client.Models;
+using Client.Helpers;
+
+namespace Client.Controllers
+{
+    public class OrdersController : Controller
+    {
+
+        private readonly IHttpHelper _httpHelper;
+
+        public OrdersController(IHttpHelper httpHelper)
+        {
+            _httpHelper = httpHelper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetProductsFromOrder(int id)
+        {
+            if (id <= 0)
+            {
+                // return an empty list
+                return Json(new List<ProductViewModel>());
+            }
+
+            var products = await _httpHelper.GetAsync<List<ProductViewModel>>($"/api/orders/{id}/products");
+            return Json(products);
+        }
+    }
+}
---
+++ Client/Models/ProductViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Client.Models
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+
+    }
+}
