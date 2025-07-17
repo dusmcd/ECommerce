2c079368c26f9cd3a0b17c1f81cde587eba2051d
dusmcd <dustinmcdowell1989@gmail.com>
2025-07-16 21:41:39 -0400

    setting up display of order details on customer profile page

--- Client/Models/OrderViewModel.cs
+++ Client/Models/OrderViewModel.cs
@@ -5,7 +5,7 @@ namespace Client.Models
     public class OrderViewModel
     {
         public int Id { get; set; }
-        public string? Status { get; set; }
+        public int Status { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime OrderDate { get; set; }
--- Client/wwwroot/js/site.js
+++ Client/wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function getProductsForOrder(id, element) {
+    $.ajax(`/Orders/GetProductsFromOrder/${id}`, {
+        dataType: "json",
+        method: "GET",
+        error: (jqXHR, textStatus, errorThrown) => {
+            console.error(errorThrown);
+        },
+        success: (data, textStatus, jqXHR) => {
+            // show data
+            element.find(".spinner-border").hide();
+            element.find(".accordion-body").append("<p>Order and product data</p>")
+            console.log("data from server: ", data);
+        }
+
+    })
+}
+
+$(function () {
+    $(".accordion-item").on("click", function(evt) {
+        const element = $(this);
+        if (evt.target.className === "accordion-button") {
+            const id = element.data("id");
+            getProductsForOrder(id, element);
+        }
+        
+    });
+})
