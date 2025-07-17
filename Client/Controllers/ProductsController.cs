c3646d8b1399e036ef2180529e4be98f54546fd6
dusmcd <dustinmcdowell1989@gmail.com>
2025-07-17 14:45:08 -0400

    displaying list of products

---
+++ Client/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Client.Models;
+using Client.Helpers;
+
+namespace Client.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        private readonly IHttpHelper _httpHelper;
+
+        public ProductsController(IHttpHelper httpHelper)
+        {
+            _httpHelper = httpHelper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var products = await _httpHelper.GetAsync<List<ProductViewModel>>("/api/products");
+            return View(products);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            if (id <= 0)
+            {
+                return View("Error");
+            }
+            var product = await _httpHelper.GetAsync<ProductViewModel>($"/api/products/{id}");
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+    }
+}
